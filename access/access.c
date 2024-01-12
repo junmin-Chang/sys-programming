65ccedc3146d90a99cc3016565f9d774c5012b54
junmin-Chang <chjm219@kookmin.ac.kr>
2024-01-12 21:33:59 +0900

    파일 시스템

---
+++ access/access.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+	if (access(argv[1], R_OK) != -1) {
+		printf("%s is readable!\n", argv[1]);
+	}
+	if (access(argv[1], W_OK) != -1) {
+		printf("%s is writable!\n", argv[1]);
+	}
+	if (access(argv[1], X_OK) != -1) {
+		printf("%s is excutable!\n", argv[1]);
+	}
+}
